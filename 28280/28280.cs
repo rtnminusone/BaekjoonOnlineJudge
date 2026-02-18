16c7a3c9e095bd83570b2c0310ca50be9eb3f174
returnminusone <returnminusone@outlook.com>
2026-02-18 20:32:06 +0900

    add 28280

---
+++ 28280/28280.cs
@@ -0,0 +1,53 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+using System.Text;
+
+class Program
+{
+	public static int N;
+	public static bool[] V;
+	public static Queue<(int, int)> Q = new Queue<(int, int)>();
+
+	public static string BFS()
+	{
+		while (Q.Count > 0)
+		{
+			var (q, w) = Q.Dequeue();
+
+			if (q == N) return w.ToString();
+
+			if (q * 2 < N + 2 && !V[q * 2])
+			{
+				Q.Enqueue((q * 2, w + 1));
+				V[q * 2] = true;
+			}
+			if (q - 1 > 0 && !V[q - 1])
+			{
+				Q.Enqueue((q - 1, w + 1));
+				V[q - 1] = true;
+			}
+		}
+
+		return "Wrong proof!";
+	}
+
+	public static void Main()
+	{
+		StringBuilder sb = new StringBuilder();
+		int t = int.Parse(Console.ReadLine());
+
+		while (t-- > 0)
+		{
+			N = int.Parse(Console.ReadLine());
+
+			Q.Clear();
+			V = new bool[N + 2];
+			Q.Enqueue((1, 0));
+			V[1] = true;
+
+			sb.AppendLine(BFS());
+		}
+
+		Console.WriteLine(sb.ToString());
+	}
+}
