e415513792fa33960cda43e0a0746bdcf33ad943
returnminusone <returnminusone@outlook.com>
2026-02-19 18:21:49 +0900

    add 14938

---
+++ 14938/14938.cs
@@ -0,0 +1,72 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N, M, K;
+	public static int[] T, dist;
+	public static List<(int, int)>[] L;
+	public static PriorityQueue<(int, int), int> PQ = new PriorityQueue<(int, int), int>();
+
+	public static void BFS()
+	{
+		while (PQ.Count > 0)
+		{
+			var (q, w) = PQ.Dequeue();
+
+			if (w > dist[q]) continue;
+
+			if (L[q] == null) continue;
+			foreach (var (nextq, nextw) in L[q])
+			{
+				if (dist[nextq] > w + nextw)
+				{
+					PQ.Enqueue((nextq, w + nextw), w + nextw);
+					dist[nextq] = w + nextw;
+				}
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		K = int.Parse(S[1]);
+		M = int.Parse(S[2]);
+		T = new int[N];
+		dist = new int[N];
+		L = new List<(int, int)>[N];
+		S = Console.ReadLine().Split();
+		for (int i = 0; i < N; i++)
+		{
+			T[i] = int.Parse(S[i]);
+		}
+		for (int i = 0; i < M; i++)
+		{
+			S = Console.ReadLine().Split();
+			int left = int.Parse(S[0]) - 1;
+			int right = int.Parse(S[1]) - 1;
+			int w = int.Parse(S[2]);
+			(L[left] ??= new List<(int, int)>()).Add((right, w));
+			(L[right] ??= new List<(int, int)>()).Add((left, w));
+		}
+		int R = int.MinValue;
+		for (int i = 0; i < N; i++)
+		{
+			Array.Fill(dist, int.MaxValue);
+			PQ.Enqueue((i, 0), 0);
+			dist[i] = 0;
+
+			BFS();
+
+			int r = 0;
+			for (int j = 0; j < N; j++)
+			{
+				if (dist[j] <= K) r += T[j];
+			}
+			R = Math.Max(R, r);
+		}
+
+		Console.WriteLine(R);
+	}
+}
