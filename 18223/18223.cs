aba5f5edabeefbafbdf8b535bea50932df818582
returnminusone <returnminusone@outlook.com>
2026-02-24 09:23:07 +0900

    add 18223

---
+++ 18223/18223.cs
@@ -0,0 +1,64 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N, M, K;
+	public static int[] dist;
+	public static List<(int, int)>[] L;
+	public static PriorityQueue<(int, int), int> PQ = new PriorityQueue<(int, int), int>();
+
+	public static void BFS()
+	{
+		while (PQ.Count > 0)
+		{
+			var (q, w) = PQ.Dequeue();
+
+			if (w > dist[q]) continue;
+
+			if (L[q] == null) continue;
+			foreach (var (nextq, nextw) in L[q])
+			{
+				if (dist[nextq] > w + nextw)
+				{
+					PQ.Enqueue((nextq, w + nextw), w + nextw);
+					dist[nextq] = w + nextw;
+				}
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		M = int.Parse(S[1]);
+		K = int.Parse(S[2]) - 1;
+		dist = new int[N];
+		Array.Fill(dist, int.MaxValue);
+		L = new List<(int, int)>[N];
+		for (int i = 0; i < M; i++)
+		{
+			S = Console.ReadLine().Split();
+			int left = int.Parse(S[0]) - 1;
+			int right = int.Parse(S[1]) - 1;
+			int w = int.Parse(S[2]);
+			(L[left] ??= new List<(int, int)>()).Add((right, w));
+			(L[right] ??= new List<(int, int)>()).Add((left, w));
+		}
+
+		PQ.Enqueue((0, 0), 0);
+		dist[0] = 0;
+		BFS();
+		int R1 = dist[N - 1];
+		int R2 = dist[K];
+
+		PQ.Clear();
+		PQ.Enqueue((K, 0), 0);
+		Array.Fill(dist, int.MaxValue);
+		dist[K] = 0;
+		BFS();
+		R2 += dist[N - 1];
+
+		Console.WriteLine(R1 < R2 ? "GOOD BYE" : "SAVE HIM");
+	}
+}
