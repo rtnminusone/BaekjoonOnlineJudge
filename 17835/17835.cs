8eaa71fee6eea4ea8cc4121033d8807af23b638b
returnminusone <returnminusone@outlook.com>
2026-02-24 10:45:58 +0900

    add 17835

---
+++ 17835/17835.cs
@@ -0,0 +1,68 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N, M, K;
+	public static long[] dist;
+	public static List<(int, int)>[] L;
+	public static PriorityQueue<(int, long), long> PQ = new PriorityQueue<(int, long), long>();
+
+	public static void Dijkstra()
+	{
+		while (PQ.Count > 0)
+		{
+			var (q, w) = PQ.Dequeue();
+
+			if (w > dist[q]) continue;
+
+			if (L[q] == null) continue;
+			foreach (var (nextq, nextw) in L[q])
+			{
+				if (dist[nextq] > w + nextw)
+				{
+					PQ.Enqueue((nextq, w + nextw), w + nextw);
+					dist[nextq] = w + nextw;
+				}
+			}
+		}
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		M = int.Parse(S[1]);
+		K = int.Parse(S[2]);
+		dist = new long[N];
+		Array.Fill(dist, long.MaxValue);
+		L = new List<(int, int)>[N];
+		for (int i = 0; i < M; i++)
+		{
+			S = Console.ReadLine().Split();
+			int left = int.Parse(S[1]) - 1;
+			int right = int.Parse(S[0]) - 1;
+			int w = int.Parse(S[2]);
+			(L[left] ??= new List<(int, int)>()).Add((right, w));
+		}
+		S = Console.ReadLine().Split();
+		for (int i = 0; i < K; i++)
+		{
+			int k = int.Parse(S[i]) - 1;
+			PQ.Enqueue((k, 0), 0);
+			dist[k] = 0;
+		}
+		Dijkstra();
+		int r = -1;
+		long val = int.MinValue;
+		for (int i = 0; i < N; i++)
+		{
+			if (val < dist[i])
+			{
+				r = i + 1;
+				val = dist[i];
+			}
+		}
+
+		Console.WriteLine(r + "\n" + val);
+	}
+}
