f8e393e640e620f13d80b993c2bec0a392b49241
returnminusone <returnminusone@outlook.com>
2026-02-27 00:47:02 +0900

    add 33656

---
+++ 33656/33656.cs
@@ -0,0 +1,83 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public struct Pos
+	{
+		public int x;
+		public int y;
+
+		public Pos(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public static int N, M;
+	public static char[,] T;
+	public static bool[,] V;
+	public static Queue<Pos> Q = new Queue<Pos>();
+
+	public static int[] dx = { -1, 0, 1, 0 };
+	public static int[] dy = { 0, -1, 0, 1 };
+
+	public static bool Create(int x, int y, out Pos pos)
+	{
+		pos = default;
+
+		if (x < 0 || x >= N || y < 0 || y >= M) return false;
+		if (T[x, y] == '.' || V[x, y]) return false;
+
+		pos = new Pos(x, y);
+
+		return true;
+	}
+
+	public static int BFS()
+	{
+		int r = 0;
+
+		while (Q.Count > 0)
+		{
+			Pos p = Q.Dequeue();
+
+			r++;
+
+			for (int i = 0; i < 4; i++)
+			{
+				if (Create(p.x + dx[i], p.y + dy[i], out Pos pos))
+				{
+					Q.Enqueue(pos);
+					V[pos.x, pos.y] = true;
+				}
+			}
+		}
+
+		return r;
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		N = int.Parse(S[0]);
+		M = int.Parse(S[1]);
+		T = new char[N, M];
+		V = new bool[N, M];
+		for (int i = 0; i < N; i++)
+		{
+			S[0] = Console.ReadLine();
+			for (int j = 0; j < M; j++)
+			{
+				T[i, j] = S[0][j];
+				if (T[i, j] == 'S')
+				{
+					Q.Enqueue(new Pos(i, j));
+					V[i, j] = true;
+				}
+			}
+		}
+
+		Console.WriteLine(BFS());
+	}
+}
