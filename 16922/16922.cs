df9e928cb9cdae49348485486662207d71f3ac48
returnminusone <returnminusone@outlook.com>
2026-03-01 05:37:32 +0900

    add 16922

---
+++ 16922/16922.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N;
+	public static HashSet<int> D = new HashSet<int>();
+
+	public static void Main()
+	{
+		N = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i <= N; i++)
+		{
+			for (int j = 0; j + i <= N; j++)
+			{
+				for (int k = 0; k + j + i <= N; k++)
+				{
+					int l = N - (i + j + k);
+					D.Add(i * 1 + j * 5 + k * 10 + l * 50);
+				}
+			}
+		}
+
+		Console.WriteLine(D.Count);
+	}
+}
