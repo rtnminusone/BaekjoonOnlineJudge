cd39de56b3271a58affa47205ac34dc4e551fcea
returnminusone <returnminusone@outlook.com>
2026-03-01 16:38:01 +0900

    add 1927

---
+++ 1927/1927.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+using System.Text;
+
+class Program
+{
+	public static PriorityQueue<int, int> PQ = new PriorityQueue<int, int>();
+
+	public static void Main()
+	{
+		StringBuilder sb = new StringBuilder();
+		int N = int.Parse(Console.ReadLine());
+		for (int i = 0; i < N; i++)
+		{
+			int s = int.Parse(Console.ReadLine());
+			if (s == 0) sb.AppendLine(PQ.Count == 0 ? "0" : PQ.Dequeue().ToString());
+			else PQ.Enqueue(s, s);
+		}
+
+		Console.WriteLine(sb.ToString());
+	}
+}
