7c9a77a1f903a916a5e0a540cccec6d4b348c776
returnminusone <returnminusone@outlook.com>
2026-03-01 20:24:35 +0900

    add 19949

---
+++ 19949/19949.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable CS8600, CS8601, CS8602, CS8603, CS8604, CS8618, CS8620
+
+class Program
+{
+	public static int N = 10, R = 0;
+	public static int[] T = new int[N];
+
+	public static void DFS(int depth, int cur, int before, bool flg)
+	{
+		if (depth == N)
+		{
+			if (cur >= 5) R++;
+			return;
+		}
+
+		for (int i = 1; i <= 5; i++)
+		{
+			if (flg && i == before) continue;
+			DFS(depth + 1, cur + (i == T[depth] ? 1 : 0), i, before == i);
+		}
+	}
+
+	public static void Main()
+	{
+		string[] S = Console.ReadLine().Split();
+		for (int i = 0; i < N; i++)
+		{
+			T[i] = int.Parse(S[i]);
+		}
+
+		DFS(0, 0, 0, false);
+
+		Console.WriteLine(R);
+	}
+}
